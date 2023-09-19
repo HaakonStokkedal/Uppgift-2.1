236e9db40bb1b7e6a579fa59ab4c57e374cc1aea
hast06001 <hast06001@E-PW024NW1>
2023-09-19 21:41:58 +0200

    Add project files.

---
+++ Uppgift 2.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uppgift_2._1
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Vilken är din favoritbok?");
+            string bok = Console.ReadLine();
+            Console.WriteLine("Din favorit bok är " + bok + ".");
+
+            Console.ReadKey();
+        }
+    }
+}
